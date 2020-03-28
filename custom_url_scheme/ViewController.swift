7a87eea15cdac484c89afc5e2f0ca6c0e775cf76
shinseunghyun <paigeshin1991gmail.com>
2020-03-28 14:42:46 +0900

    info.plist만 보면 됨

---
+++ custom_url_scheme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  custom_url_scheme
+//
+//  Created by shin seunghyun on 2020/03/27.
+//  Copyright © 2020 shin seunghyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
